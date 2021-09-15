2cc48fa8ce75859fe21d6e12b899a85eb9010ad2
gkvermilion <yarovoy1999@bk.ru>
2021-09-16 02:42:05 +1000

    email is quite done

--- server/dtos/userDto.js
+++ server/dtos/userDto.js
@@ -1,11 +1,13 @@
 module.exports = class UserDto {
-    email;
     id;
+    email;
     isActivated;
+    role;
 
     constructor(model) {
         this.email = model.email;
         this.isActivated = model.isActivated;
         this.id = model.id;
+        this.role = model.role;
     }
 }
--- server/services/userService.js
+++ server/services/userService.js
@@ -3,13 +3,14 @@ const bcrypt = require('bcrypt')
 const uuid = require('uuid')
 const mailService = require('./mailService')
 const tokenService = require('./tokenService')
-const UserDto = require("../dtos/userDto");
+const UserDto = require('../dtos/userDto');
+const ApiError = require('../error/ApiError')
 
 class userService {
     async registration(email, password) {
         const candidate = await User.findOne({where: {email}});
         if (candidate) {
-            throw new Error(`Пользователь с почтовым адресом ${email} уже существует`)
+            throw ApiError.badRequest(`Пользователь с почтовым адресом ${email} уже существует`)
         }
         const hashPassword = await bcrypt.hash(password, 3);
         const activationLink = uuid.v4();
@@ -18,7 +19,7 @@ class userService {
         await mailService.sendActivationMail(email, `${process.env.API_URL}/api/user/activate/${activationLink}`);
         // DTO = data transfer object
         const userDto = new UserDto(user); // id, email, iActivated
-        const tokens = tokenService.generateTokens({...UserDto})
+        const tokens = tokenService.generateTokens({...userDto})
         await tokenService.saveToken(userDto.id, tokens.refreshToken);
 
         return {
@@ -30,10 +31,25 @@ class userService {
     async activate(activationLink) {
         const user = await User.findOne({where: {activationLink}});
         if (!user) {
-            throw new Error(`Некорректная ссылка для активации`)
+            throw ApiError.badRequest(`Некорректная ссылка для активации`)
         }
         user.isActivated = true;
         await user.save();
+    }
+
+    async login(email, password) {
+        const user = await User.findOne({where: {email}});
+        if (!user) {
+            throw ApiError.badRequest('Пользователя с таким email не существует');
+        }
+        let isPassEquals = await bcrypt.compareSync(password, user.password);
+        if (!isPassEquals) {
+            throw ApiError.badRequest('Неверный пароль');
+        }
+        const userDto = new UserDto(user)
+        const tokens = tokenService.generateTokens({...userDto})
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+        return {...tokens, user: userDto};
     }
 }
 
