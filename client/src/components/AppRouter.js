6a84ff47bc282e4fe2a50115539b5726c7db105d
rast22 <rastrus23@gmail.com>
2021-09-15 02:17:27 +0200

    commit by rast

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,11 +1,15 @@
 import React from "react";
 import {Switch, Route, Redirect} from 'react-router-dom'
+import {publicRoutes} from "../routes";
 
 const AppRouter = () => {
+    const isAuth = false
     return (
-        <div>
-
-        </div>
+        <Switch>
+            {publicRoutes.map(({path, component}) =>
+            <Route key={path} path={path} component={component} exact/>
+            )}
+        </Switch>
     )
 }
 
